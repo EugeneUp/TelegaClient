a4dc92feed21b2ca2d3001ea2be13813f458c4a8
EugeneUp <eugeneup.official@gmail.com>
2021-03-21 21:46:39 +0300

    deleted useless dependencies

--- build/scripts/TelegaClient.bat
+++ build/scripts/TelegaClient.bat
@@ -67,7 +67,7 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\TelegaClient-1.0-SNAPSHOT.jar;%APP_HOME%\lib\commons-lang3-3.3.2.jar;%APP_HOME%\lib\javafx-fxml-15.0.1-win.jar;%APP_HOME%\lib\javafx-controls-15.0.1-win.jar;%APP_HOME%\lib\javafx-controls-15.0.1.jar;%APP_HOME%\lib\javafx-graphics-15.0.1-win.jar;%APP_HOME%\lib\javafx-graphics-15.0.1.jar;%APP_HOME%\lib\javafx-base-15.0.1-win.jar;%APP_HOME%\lib\javafx-base-15.0.1.jar
+set CLASSPATH=%APP_HOME%\lib\TelegaClient-1.0-SNAPSHOT.jar;%APP_HOME%\lib\javafx-fxml-15.0.1-win.jar;%APP_HOME%\lib\javafx-controls-15.0.1-win.jar;%APP_HOME%\lib\javafx-controls-15.0.1.jar;%APP_HOME%\lib\javafx-graphics-15.0.1-win.jar;%APP_HOME%\lib\javafx-graphics-15.0.1.jar;%APP_HOME%\lib\javafx-base-15.0.1-win.jar;%APP_HOME%\lib\javafx-base-15.0.1.jar
 
 
 @rem Execute TelegaClient
